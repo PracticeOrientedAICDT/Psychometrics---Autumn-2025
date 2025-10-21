7c905dffa41819007fe97c1b4f6fe68c269a8235
noah-barker-uni <og25914@bristol.ac.uk>
2025-10-21 13:27:21 +0100

    Added a file to simulate data given IRT parameters and modified MIRT.r to read this data and plot trace lines.

--- MIRT.r
+++ MIRT.r
@@ -2,23 +2,13 @@ library(tidyverse)
 library(mirt)
 library(ggmirt)
 
-# Simulate some data
-data <- sim_irt(500, 8, seed = 123)
-
-# Save the simulated data to a CSV file
-write.csv(data, "simulated_data.csv", row.names = FALSE)
-
-# Read the data back from the CSV file
-data <- read.csv("simulated_data.csv")
-
-# Run IRT model with mirt
+# Load data
+data <- read.csv("irt_data.csv")
 mod <- mirt(data, 1, itemtype = "2PL", verbose = FALSE)
 
 # Plot item-person map
-#itempersonMap(mod)
+itempersonMap(mod)
+tracePlot(mod)
+tracePlot(mod, facet = F, legend = T)
 
-# Plot item information curves
-#itemInfoPlot(mod, facet = T)
-
-
-summary(mod)
+coef(mod)
